e6a713a676fad208353e568e2e784146b57d2f90
Simon Hawkin <simon.hawkin@gmail.com>
2019-10-28 17:28:25 -0400

    Create custom.el

---
+++ custom.el
@@ -0,0 +1,18 @@
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(case-fold-search nil)
+ '(default-input-method "cyrillic-yawerty")
+ '(global-font-lock-mode nil nil (font-lock))
+ '(save-place t nil (saveplace))
+ '(show-paren-mode nil)
+ '(transient-mark-mode t)
+ '(uniquify-buffer-name-style (quote forward) nil (uniquify)))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(font-lock-comment-face ((((class color) (min-colors 8) (background dark)) (:foreground "red")))))
